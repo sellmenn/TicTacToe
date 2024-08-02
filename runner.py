2a588f6438537e49b7024fd56aee1aedead92516
Ariq Koh <105147577+sellmenn@users.noreply.github.com>
2024-08-03 01:05:43 +0800

    Fixed lag when selecting 'X' as player

--- runner.py
+++ runner.py
@@ -4,47 +4,119 @@ from copy import deepcopy
 from time import sleep
 from player import *
 
-# Window Size
-X_RES = 1280
-Y_RES = 720
-
 def runner():
-    board = Board()
     pygame.init()
-    screen = pygame.display.set_mode((X_RES, Y_RES))
+    screen = pygame.display.set_mode((1280, 720))
     pygame.display.set_caption("Tic-Tac-Toe")
     clock = pygame.time.Clock()
+
+    font = pygame.font.Font('freesansbold.ttf', 25)
+    option_o = font.render('Play as O', True, "white")
+    option_x = font.render('Play as X', True, "white")
+    retry = font.render('Retry', True, "white")
+    quit = font.render('Quit', True, "white")
+
+    o_rect = option_o.get_rect(center=(420, 720/2))
+    x_rect = option_x.get_rect(center=(860, 720/2))
+    retry_rect = retry.get_rect(center=(420, 720/2))
+    quit_rect = quit.get_rect(center=(860, 720/2))
+
     running = True
-    o_list, x_list = list(), list()
-    turn = "O"
+
     while running:
-        screen.fill("black")
-        if not board.end_game():
+        turn = None
+        player = None
+
+        # Prompt for O or X
+        while turn is None:
+            screen.fill("black")
+            screen.blit(option_o, o_rect)
+            screen.blit(option_x, x_rect)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    pygame.quit()
+                    return
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    coordinate = get_coordinate(pygame.mouse.get_pos())
-                    center_pos = get_center_pos(coordinate)
-                    if center_pos not in o_list and center_pos not in x_list:
-                        if turn == "O":
-                            o_list.append(center_pos)
-                            board.make_move(coordinate)
-                            turn = "X"
-            if turn == "X" and not board.end_game():
+                    x, y = pygame.mouse.get_pos()
+                    if o_rect.collidepoint(x, y):
+                        turn = "O"
+                        player = turn
+                    elif x_rect.collidepoint(x, y):
+                        turn = "X"
+                        player = "X"
+
+            pygame.display.flip()
+            clock.tick(30)
+
+        board = Board()
+        o_list, x_list = [], []
+
+        while not board.end_game() and running:
+            screen.fill("black")
+
+            if turn != player:
                 ai_move = best_move(board)
                 board.make_move(ai_move)
-                x_list.append(get_center_pos(ai_move))
-                turn = "O"
+                if player == "O":
+                    turn = "O"
+                    x_list.append(get_center_pos(ai_move))
+                else:
+                    turn = "X"
+                    o_list.append(get_center_pos(ai_move))
+            else:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        return
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        coordinate = get_coordinate(pygame.mouse.get_pos())
+                        center_pos = get_center_pos(coordinate)
+                        if center_pos not in o_list and center_pos not in x_list:
+                            if turn == player:
+                                if player == "O":
+                                    o_list.append(center_pos)
+                                    turn = "X"
+                                else:
+                                    x_list.append(center_pos)
+                                    turn = "O"
+                                board.make_move(coordinate)
+                                
+            draw_grid(screen)
+            draw_o(screen, o_list)
+            draw_x(screen, x_list)
+            pygame.display.flip()
+            clock.tick(30)
+
+        # Show winner
+        utility = get_utility(board)
+        if utility == 1:
+            winner = "O"
+        elif utility == -1:
+            winner = "X"
         else:
-            sleep(1)
-            running = False
-        draw_grid(screen)
-        draw_o(screen, o_list)
-        draw_x(screen, x_list)
-        pygame.display.flip()
-        clock.tick(30)
-    pygame.quit()
+            winner = "Draw"
+
+        while running:
+            screen.fill("black")
+            text = font.render(f'Winner: {winner}', True, "white")
+            text_rect = text.get_rect(center=(1280/2, 720/2))
+            screen.blit(text, text_rect)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                    if retry_rect.collidepoint(x, y):
+                        break
+                    elif quit_rect.collidepoint(x, y):
+                        pygame.quit()
+                        return
+
+            pygame.display.flip()
+            clock.tick(30)
 
 def draw_grid(screen):
     x, y = screen.get_size()
@@ -66,6 +138,9 @@ def draw_x(screen, positions):
         pygame.draw.line(screen, "white", (x-50, y-50), (x+50, y+50), 1)
         pygame.draw.line(screen, "white", (x-50, y+50), (x+50, y-50), 1)
 
+"""
+Function which if passed a cursor position (X, Y), returns the corresponding coordinate on Board object as a tuple
+"""
 def get_coordinate(position):
     x, y = position
     coord_x, coord_y = 0, 0
@@ -83,6 +158,9 @@ def get_coordinate(position):
         coord_x = 2
     return coord_x, coord_y
 
+"""
+Function which if passed a coordinate on Board, object, returns the center cursor position as a tuple
+"""
 def get_center_pos(coordinate):
     x, y = 0, 0
     coord_x, coord_y = coordinate
